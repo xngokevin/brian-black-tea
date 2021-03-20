00c6010a5fe9f9b56c1b9ab6c54451216c9cedcd
Kano Ngo <kano@inphantry.com>
2021-03-19 22:32:45 -0700

    Initialize react project with configurations

---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "./../../parts/header";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className={"container"}>
+        <Header></Header>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/parts/header/index.js
@@ -0,0 +1,9 @@
+function Header(props) {
+  return (
+    <div>
+      <span>Hello World!</span>
+    </div>
+  );
+}
+
+export default Header;
