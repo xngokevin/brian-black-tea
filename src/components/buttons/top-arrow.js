3284b34f11fd32c90051ffa3ee80b795f56b0b9e
Kano Ngo <kano@inphantry.com>
2021-03-25 10:42:49 -0700

    Hide top arrow on top page

--- src/components/buttons/top-arrow.js
+++ src/components/buttons/top-arrow.js
@@ -1,10 +1,39 @@
 import topArrowIcon from "./../../assets/images/icons/ic-top-arrow.svg";
+import React from "react";
 
 function TopArrow(props) {
+  const [showTopArrow, setShowTopArrow] = React.useState(false);
+
+  React.useEffect(() => {
+    const updateNavbar = () => {
+      if (
+        document.documentElement.scrollTop > 19 ||
+        document.body.scrollTop > 19
+      ) {
+        setShowTopArrow(true);
+      } else if (
+        document.documentElement.scrollTop < 20 ||
+        document.body.scrollTop < 20
+      ) {
+        setShowTopArrow(false);
+      }
+    };
+
+    window.addEventListener("scroll", updateNavbar);
+
+    return function cleanup() {
+      window.removeEventListener("scroll", updateNavbar);
+    };
+  });
+
   return (
-    <a className={"c-top-arrow"} onClick={props.onClick}>
-      <img src={topArrowIcon} alt={"icon"} />
-    </a>
+    <>
+      {showTopArrow && (
+        <a className={"c-top-arrow"} onClick={props.onClick}>
+          <img src={topArrowIcon} alt={"icon"} />
+        </a>
+      )}
+    </>
   );
 }
 
